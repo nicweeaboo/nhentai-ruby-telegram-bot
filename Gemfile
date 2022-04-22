a817da193ebe7511f6ce41a230f4f708c4252630
nicweeaboo <gregory.mayer1998@gmail.com>
2022-04-22 00:33:37 -0300

    first commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org' do
+	ruby '2.7.1'
+  	gem 'telegram-bot-ruby'
+    gem 'json'
+    gem 'nhentai-api'
+    gem 'net-http-persistent', '~> 2.9', '>= 2.9.4'
+end
---
+++ lib/bot.rb
@@ -0,0 +1,63 @@
+require 'telegram/bot'
+require 'nhentai-api'
+
+class Bot
+
+  def initialize
+
+    token = ENV['TELEGRAM_BOT_H']
+    
+    Telegram::Bot::Client.run(token) do |bot|
+      bot.listen do |message|
+        case message
+        when Telegram::Bot::Types::Message
+
+          case message.text
+    
+          when '/today', '/today@sadistic_oneesan_ruby_bot'
+            today_doujinshi = Doujinshi.new(Time.now.strftime("%d%m%y"))
+            if today_doujinshi.exists?
+              today_doujinshi_tags = []
+              today_doujinshi.tags.each do |tag|
+                today_doujinshi_tags << tag.name.gsub!('<span class="name">','')
+              end
+              bot.api.send_message(chat_id: message.chat.id, 
+                text: "<b>Nhentai doujinshi based on today's date #{Time.now.strftime("%d/%m/%y")} (#{Time.now.strftime("%d%m%y")}) </b>\n<a href='#{today_doujinshi.cover}'><b>TITLE: #{today_doujinshi.title}</b></a>\n<b>TAGS: #{today_doujinshi_tags[0..12].join(', ')}</b>\n<a href='https://nhentai.net/g/#{today_doujinshi.id}'><b>READ NOW</b></a>",
+                parse_mode: "HTML")
+            else
+              bot.api.send_message(chat_id: message.chat.id, text: "Couldn't find any ;/" )
+            end
+          when /\/code/
+            code = message.text.delete('^0-9')
+            if message.text == "/code #{code}"
+              doujinshi=Doujinshi.new(code)
+              if doujinshi.exists?
+                tags = []
+                doujinshi.tags.each do |tag|
+                  tags << tag.name.gsub!('<span class="name">','')
+                end
+                bot.api.send_message(chat_id: message.chat.id, text: "<a href='#{doujinshi.cover}'><b>TITLE: #{doujinshi.title}</b></a>\n<b>TAGS: #{tags[0..12].join(', ')}</b>\n<a href='https://nhentai.net/g/#{doujinshi.id}'><b>READ NOW</b></a>",
+                parse_mode: "HTML")
+              else
+                bot.api.send_message(chat_id: message.chat.id, text: "Couldn't find any ;/" )
+              end
+            end
+            
+          when '/instagram', '/instagram@sadistic_oneesan_ruby_bot'
+            bot.api.send_message(chat_id: message.chat.id,
+              text: "<b>Página Oficial 1 Real a Hora no instagram</b> 🌚\n👉 https://www.instagram.com/1realahora/",
+              parse_mode: "HTML")
+
+          when '/facebook', '/facebook@sadistic_oneesan_ruby_bot'
+            bot.api.send_message(chat_id: message.chat.id, 
+              text: "<b>Página Oficial 1 Real a Hora no facebook</b> 🌚\n👉 https://pt-br.facebook.com/1realahora/",
+              parse_mode: "HTML")
+              
+          end
+        end
+      end
+    end
+
+  end
+
+end
